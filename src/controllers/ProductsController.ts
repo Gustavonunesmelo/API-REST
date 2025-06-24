16bdd0ce8de4523e7285e20e53b82a37f13cbae6
Gustavo nunes melo <156378462+Gustavonunesmelo@users.noreply.github.com>
2025-06-24 15:34:54 -0300

    Criando a class AppError.

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { AppError } from "../utils/AppError";
 
 export class ProductsController {
 /** 
@@ -17,6 +18,8 @@ export class ProductsController {
 
   create(request: Request, response: Response) {
     const {name, price } = request.body
+
+    // throw new AppError("Erro ao tentar criar um produto!")
     
     response.status(201).json({name, price, user_id: request.user_id })
   }
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response, NextFunction } from "express"
 import { routes } from "./routes/index"
-import { error } from "console"
+import { AppError } from "./utils/AppError"
 
 const PORT = 3333
 
 const app = express()
+
 app.use(express.json())
 
 app.use(routes)
 
-app.use((error:any, request: Request, response: Response) => {
+app.use((error: any, request: Request, response: Response, next: NextFunction ) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message })
+  }
+
   response.status(500).json({ message: error.message})
 })
 
