9380d81a6873b476d6d0bebfe2afbae4f99c2103
Gustavo nunes melo <156378462+Gustavonunesmelo@users.noreply.github.com>
2025-06-24 14:13:01 -0300

    Tratando exceções.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express, { response } from "express"
+import express, { Request, Response } from "express"
 import { routes } from "./routes/index"
+import { error } from "console"
 
 const PORT = 3333
 
@@ -7,5 +8,9 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error:any, request: Request, response: Response) => {
+  response.status(500).json({ message: error.message})
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
